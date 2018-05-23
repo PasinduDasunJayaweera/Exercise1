6dc9c5b1b653a97b90841ce8f705e4215efd1499
Pasindu Dasun <dasun.pasindu@yahoo.com>
2018-05-23 17:53:40 +0200

    updated sentence

--- src/MyFirstPrj/Exercise1.java
+++ src/MyFirstPrj/Exercise1.java
@@ -5,7 +5,7 @@ public class Exercise1 {
 
 	public static void main(String [ ] args) {
 		
-		System.out.println("My name is Pasindu");
+		System.out.println("My name is Pasi");
 		System.out.println("I am a master student");
 		System.out.println("I will work hard and complete my masters successfully");
 		System.out.println("I will not copy the assignments, exercises or the final mini-project");
